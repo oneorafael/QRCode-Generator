61795008da85f05ae651c380245d338c816b004a
Rafael Oliveira <oliveirafael@icloud.com>
2020-11-04 00:02:58 -0300

    Initial Commit

---
+++ QRCode Generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QRCode Generator
+//
+//  Created by Rafael Oliveira on 04/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
