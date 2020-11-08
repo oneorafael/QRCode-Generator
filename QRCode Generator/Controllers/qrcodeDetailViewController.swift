ff0c7f9437b1207a480fc05a14a210c4dfd7a89b
Rafael Oliveira <oliveirafael@icloud.com>
2020-11-07 21:43:59 -0300

    - generate QR Code ; - add title on "save button" and "back button"

--- QRCode Generator/Controllers/qrcodeDetailViewController.swift
+++ QRCode Generator/Controllers/qrcodeDetailViewController.swift
@@ -10,24 +10,37 @@ import UIKit
 class qrcodeDetailViewController: UIViewController {
     
     @IBOutlet weak var qrCodeContentTitle: UITextField!
+    @IBOutlet weak var qrCodeImage: UIImageView!
     var selectedItem : Item?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let newItem = selectedItem
-        qrCodeContentTitle.text = newItem?.title
+        let finalContent = newItem?.title
+        qrCodeContentTitle.text = finalContent
+        let qrcode = generateQRCode(from: finalContent!)
+        qrCodeImage.image = qrcode
+        
     }
     
 
-    /*
-    // MARK: - Navigation
+    
+    func generateQRCode(from string: String) -> UIImage? {
+        let data = string.data(using: String.Encoding.ascii)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let filter = CIFilter(name: "CIQRCodeGenerator") {
+            filter.setValue(data, forKey: "inputMessage")
+            let transform = CGAffineTransform(scaleX: 10, y: 10)
+
+            if let output = filter.outputImage?.transformed(by: transform) {
+                return UIImage(ciImage: output)
+            }
+        }
+        return nil
     }
-    */
-
+    @IBAction func confirmButtonPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+        
+    }
 }
